a311ae9d0cb00d224f32bbbf21d7cf5d4262d1f6
Abdoulaye Dieme <palaye122@gmail.com>
2025-08-20 22:32:21 +0000

    Correction de l'import de createClient dans supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,4 +1,5 @@
-// src/lib/supabaseClient.import { createClient } from '@supabase/supabase-js';
+// src/lib/supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
